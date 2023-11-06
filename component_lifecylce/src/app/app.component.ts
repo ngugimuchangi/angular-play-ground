6d7003e007fac2779114efa02217f88975472dc9
ngugimuchangi <tech.curious14@gmail.com>
2023-11-06 13:08:38 +0300

    addition of lifecylce hooks to parent component to track parent-child lifecycle order

--- component_lifecylce/src/app/app.component.ts
+++ component_lifecylce/src/app/app.component.ts
@@ -1,3 +1,18 @@
+/**
+ * NB: Logs are in the order of the lifecycle hooks. They are key in tracing the lifecycle of
+ * the component and its children
+ *
+ * Observation so far:
+ * - Child components are initialized after the parent component
+ * - View children are initialized after the content children
+ *
+ * Key thing to remember: `@ViewChild` and `@ContentChild` are available in the ngAfterViewInit
+ * and ngAfterContentInit hooks respectively
+ *
+ * Therefore, the children components are available in the `ngAfterViewInit` and
+ * `ngAfterContentInit` hooks of the parent component
+ */
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -14,8 +29,29 @@ export class AppComponent {
     console.log('%cAppComponent constructor', 'color: salmon');
   }
 
+  ngOnInit(): void {
+    console.log('%cAppComponent ngOnInit', 'color: salmon');
+  }
+
+  ngDoCheck(): void {
+    console.log('%cAppComponent ngDoCheck', 'color: salmon');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('%cAppComponent ngAfterContentInit', 'color: salmon');
+    this.inputVal = 'Hello';
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('%cAppComponent ngAfterContentChecked', 'color: salmon');
+  }
+
   ngAfterViewInit(): void {
     console.log('%cAppComponent ngAfterViewInit', 'color: salmon');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('%cAppComponent ngAfterViewChecked', 'color: salmon');
   }
 
   onClick(inputEl: HTMLInputElement) {
