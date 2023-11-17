eb3859d6dc7295e59c6d4ab896644c4946522c2b
ngugimuchangi <tech.curious14@gmail.com>
2023-11-17 14:42:09 +0300

    renamed test_app t e-kart

--- test_app/src/app/components/container/featured-brands/featured-brands.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'featured-brands',
-  templateUrl: './featured-brands.component.html',
-  styleUrls: ['./featured-brands.component.scss'],
-})
-export class FeaturedBrandsComponent {}
--- test_app/src/app/directives/disable-product.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisableProductDirective } from './disable-product.directive';
-
-describe('DisableProductDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DisableProductDirective();
-    expect(directive).toBeTruthy();
-  });
-});
