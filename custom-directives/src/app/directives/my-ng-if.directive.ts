3ddfaccf90d68fdf74075d11943c6cf68cb8483f
ngugimuchangi <tech.curious14@gmail.com>
2023-11-22 18:50:24 +0300

    add alternative view input option and rendering logic

--- custom-directives/src/app/directives/my-ng-if.directive.ts
+++ custom-directives/src/app/directives/my-ng-if.directive.ts
@@ -1,19 +1,48 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import {
+  Directive,
+  Input,
+  OnChanges,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 
+/**
+ * @description
+ * Simple implementation of ngIf directive
+ * @example
+ * <div *myNgIf="condition; else altView">Main View</div>
+ */
 @Directive({
   selector: '[myNgIf]',
 })
-export class MyNgIfDirective {
-  @Input('myNgIf') set display(condition: boolean) {
-    if (condition) {
-      this.template.createEmbeddedView(this.view);
-    } else {
-      this.template.clear();
-    }
-  }
+export class MyNgIfDirective implements OnChanges {
+  // To keep track of the current view and prevent unnecessary view creation
+  currView?: 'main' | 'alt';
+
+  @Input('myNgIf')
+  display: boolean = false;
+
+  @Input('myNgIfElse')
+  altView: TemplateRef<any> | null = null;
 
   constructor(
-    private view: TemplateRef<unknown>,
-    private template: ViewContainerRef
+    private readonly template: TemplateRef<unknown>,
+    private readonly viewContainerRef: ViewContainerRef
   ) {}
+
+  /** Create view based on condition and current view */
+  ngOnChanges() {
+    if (this.display && this.currView !== 'main')
+      this.createEmbeddedView(this.template);
+
+    if (!this.display && this.altView && this.currView !== 'alt')
+      this.createEmbeddedView(this.altView);
+  }
+
+  /** Create embedded view and set current view */
+  createEmbeddedView(templateRef: TemplateRef<any>): void {
+    this.viewContainerRef.clear();
+    this.viewContainerRef.createEmbeddedView(templateRef);
+    this.currView = templateRef === this.template ? 'main' : 'alt';
+  }
 }
