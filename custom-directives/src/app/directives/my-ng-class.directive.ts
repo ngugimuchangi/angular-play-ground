44658291c02127c80b50b8bd522de0cd8f36cfba
ngugimuchangi <tech.curious14@gmail.com>
2023-11-22 18:49:35 +0300

    add lifecylce hook to check for undetected input object changes

--- custom-directives/src/app/directives/my-ng-class.directive.ts
+++ custom-directives/src/app/directives/my-ng-class.directive.ts
@@ -1,18 +1,45 @@
-import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  DoCheck,
+  ElementRef,
+  Input,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[myNgClass]',
+  exportAs: 'myNgClass',
 })
-export class MyNgClassDirective {
-  @Input('myNgClass') set myNgClass(classes: { [className: string]: boolean }) {
+export class MyNgClassDirective implements DoCheck {
+  private _classes: Record<string, boolean> = {};
+
+  @Input('myNgClass') set classes(classes: Record<string, boolean>) {
+    this._classes = classes;
+    this.setClass(classes);
+  }
+
+  get classes() {
+    return this._classes;
+  }
+
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
+
+  ngDoCheck() {
+    this.setClass(this.classes);
+  }
+
+  /**
+   * @description
+   * Add or remove classes based on the condition
+   * @param classes - classes to be added or removed
+   */
+  setClass(classes: Record<string, boolean>) {
     for (const className in classes) {
-      if (classes[className]) {
+      if (this.classes[className]) {
         this.renderer.addClass(this.element.nativeElement, className);
       } else {
         this.renderer.removeClass(this.element.nativeElement, className);
       }
     }
   }
-
-  constructor(private element: ElementRef, private renderer: Renderer2) {}
 }
