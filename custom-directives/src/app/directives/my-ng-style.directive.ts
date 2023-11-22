68170ecf123a11d36676c03d9d8f684995647bfc
ngugimuchangi <tech.curious14@gmail.com>
2023-11-22 19:12:22 +0300

    Check for changes in styles over directive lifecylce

--- custom-directives/src/app/directives/my-ng-style.directive.ts
+++ custom-directives/src/app/directives/my-ng-style.directive.ts
@@ -1,17 +1,67 @@
-import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[myNgStyle]',
 })
-export class MyNgStyleDirective {
-  @Input('myNgStyle') set myNgStyle(styles: { [styleName: string]: string }) {
-    for (const styleName in styles) {
-      this.renderer.setStyle(
-        this.element.nativeElement,
-        styleName,
-        styles[styleName]
-      );
-    }
+export class MyNgStyleDirective implements OnInit, DoCheck {
+  private _styles: Record<string, string> = {};
+  private _oldStyles: Record<string, string> = {};
+  private _firstChange = true;
+
+  @Input('myNgStyle') set styles(styles: Record<string, string>) {
+    this._styles = styles;
   }
+  get styles() {
+    return this._styles;
+  }
+
   constructor(private element: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.setOrRemoveStyles(this._styles);
+    this._firstChange = false;
+  }
+
+  ngDoCheck() {
+    if (!this._firstChange) this.setOrRemoveStyles(this.styles);
+  }
+
+  /**
+   * @description
+   * This method sets or removes css styles provided
+   * Old styles are removed and new styles are set
+   * @param styles - styles to be set or removed
+   */
+  setOrRemoveStyles(styles: Record<string, string>) {
+    // Set styles that are present in the new styles
+    for (const styleName in styles) {
+      if (
+        !this._oldStyles[styleName] ||
+        (this._oldStyles[styleName] &&
+          this._oldStyles[styleName] !== styles[styleName])
+      ) {
+        this.renderer.setStyle(
+          this.element.nativeElement,
+          styleName,
+          styles[styleName]
+        );
+      }
+    }
+
+    // Remove styles that are not present in the new styles
+    for (const styleName in this._oldStyles) {
+      if (!styles[styleName]) {
+        this.renderer.removeStyle(this.element.nativeElement, styleName);
+      }
+    }
+
+    this._oldStyles = { ...this.styles };
+  }
 }
